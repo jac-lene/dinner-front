a6ab2105abcad6abbf6f669aaba8575c6ad30e93
Jordan-Burgess <alfredjordan93@gmail.com>
2022-12-07 02:47:07 -0500

    Create get chats function

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,22 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Chat = () => {
+  const [isLoading, setLoading] = useState(true);
+  cons [data, setData] = useState([])
+
+  const getChats = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/chats/2/');
+      const json = await response.json();
+      setData(json)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  
   return (
     <View>
       <Text>Chat</Text>
