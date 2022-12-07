c29d8a157b1c8cc2800f4628f9089354f1c082b4
jac-lene <jaclenesini@gmail.com>
2022-12-06 22:25:52 -0500

    carousel looks good and dots added

--- components/Splash.js
+++ components/Splash.js
@@ -10,58 +10,44 @@ const Splash = () => {
 
   return (
     <SafeAreaView style={styles.appContainer}>
-    <StatusBar style="light" />
-
-    
-
 
     { carousel === 0 ?  
-    <View style={styles.titleCont}>
-    <Text style={styles.title}>Host an Event</Text>
-    <View style={styles.textCont}>
-    <View style={styles.titleCont} >
+    <View style={styles.body}>
+    <Text style={styles.header}>Host an Event</Text>
         <TouchableOpacity  onPress={() => { setCarousel(1) }} >
-            <Text style={styles.text} > Have an open seat at the table? </Text>
+            <Text style={styles.text} >Have an open seat at the table?</Text>
         </TouchableOpacity> 
-    </View> 
-    </View>
     </View>
     : null }
-  
-
-    { carousel === 1 ? 
-     <View style={styles.titleCont}>
-     <Text style={styles.title}>Grab a Seat</Text>
-     <View style={styles.textCont}>
-     <View style={styles.titleCont} >
-         <TouchableOpacity  onPress={() => { setCarousel(2) }} >
-             <Text style={styles.text} > Maybe you’re looking for that extra seat?  </Text>
-         </TouchableOpacity> 
-     </View> 
-     </View>
-     </View>
+    { carousel === 1 ?  
+    <View style={styles.body}>
+    <Text style={styles.header}>Grab a Seat</Text>
+        <TouchableOpacity  onPress={() => { setCarousel(2) }} >
+            <Text style={styles.text} >Maybe you’re looking for that extra seat? </Text>
+        </TouchableOpacity> 
+    </View>
     : null }
-
-    { carousel === 2 ? 
-     <View style={styles.titleCont}>
-     <Text style={styles.title}>Share the Experience</Text>
-     <View style={styles.textCont}>
-     <View style={styles.titleCont} >
-         <TouchableOpacity  onPress={() => { setCarousel(0) }} >
-             <Text style={styles.text} > Whether you want to display your hospitality or provide great company, we have your back in finding the right connection. </Text>
-         </TouchableOpacity> 
-     </View> 
-     </View>
-     </View>
+    { carousel === 2 ?  
+    <View style={styles.body}>
+    <Text style={styles.header}>Share the Experience</Text>
+        <TouchableOpacity  onPress={() => { setCarousel(0) }} >
+            <Text style={styles.text} >Whether you want to display your hospitality or provide great company, we have your back in finding the right connection.</Text>
+        </TouchableOpacity> 
+    </View>
     : null }
-
-
     
+    <View style={styles.dotContainer}>
+        <TouchableOpacity onPress={() => { setCarousel(0) }} style={[styles.dotStyle, {opacity: carousel === 0 ? 1 : .4 } ]}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { setCarousel(1) }} style={[styles.dotStyle, {opacity: carousel === 1 ? 1 : .4 } ]}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { setCarousel(2) }} style={[styles.dotStyle, {opacity: carousel === 2 ? 1 : .4 } ]}>
+        </TouchableOpacity>
+    </View>
     
-    
-    <View style={styles.buttonCont}>
+    <View style={styles.button}>
         <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text style={styles.button}>Sign Up</Text>
+            <Text  style={styles.buttonText}>Sign Up</Text>
         </TouchableOpacity>
     </View>
        
@@ -72,44 +58,56 @@ const Splash = () => {
 const styles = StyleSheet.create({
     appContainer: {
       flex: 1,
-      paddingHorizontal: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
-    titleCont: {
-        flex: 5,
-    },
-    title: {
-    //   flex: 2,
+    header: {
       fontSize: 40,
       fontWeight: 'bold',
-      paddingTop: 180,
-      textAlign: 'center',
-      paddingHorizontal: 60,
+      height: 240,
+    },
+    body: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 40,
     },
     text: {
-      fontSize: 19,
-      textAlign: 'center',
+      fontSize: 20,
+      textAlign: 'center'
     },
-    textCont: {
-        paddingTop: 210,
-        flex: 1, 
+    dotContainer: {
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent: 'center', //Centered horizontally
-        alignItems: 'center', //Centered vertically
+        borderRadius: 50,
+        marginHorizontal: 40,
     },
-      button: {
+    dotStyle: {
+        width: 10,
+        height: 10,
+        backgroundColor: 'gray',
+        opacity: .4,
+        borderRadius: 50,
+        margin: 2,
+        marginTop: -300,
+    },
+    button: {
+        flexDirection: 'row',
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: 'grey',
-        fontSize: 19,
-        color: '#fff',
-        textAlign: 'center',
-        paddingVertical: 8,
         borderRadius: 50,
         height: 40,
+        marginHorizontal: 40,
+        marginBottom: 220,
       },
-      buttonCont: {
-        flex: 2,
-        borderRadius: 10,
-        paddingTop: 0,
-      },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    }
   });
 
 export default Splash
